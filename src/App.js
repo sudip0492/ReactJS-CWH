b59f8ff550a67aa30daa51018755f412e9e51cc4
Sudipta Maity <sudiptamaity0492@gmail.com>
2023-05-05 01:25:34 +0530

    fix 5.5.23

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from './components/TextForm';
-import About from "./components/About";
+// import About from "./components/About";
 import { useState } from "react";
 
 function App() {
